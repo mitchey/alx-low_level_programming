40bc329efb899e6c2424c2ba5bce995d5422882b
Michael Uzor <michael.uzor@gmail.com>
2022-09-08 15:57:41 -0700

    A C script that prints a statement

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /*
  * main -serves as the entry point for the program
  *
- * return - by default returns zero if no errors otherwise returns non-zero value.
+ * return - returns zero if no errors otherwise returns non-zero value.
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
