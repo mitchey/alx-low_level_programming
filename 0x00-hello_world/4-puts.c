69715686654ee41adb95c52875eb3c0b0e19f96c
Michael Uzor <michael.uzor@gmail.com>
2022-09-08 15:52:59 -0700

    A C script that prints a statement

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+/*
+ * main -serves as the entry point for the program
+ *
+ * return - by default returns zero if no errors otherwise returns non-zero value.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
