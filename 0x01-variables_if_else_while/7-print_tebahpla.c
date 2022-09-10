fe3e6f04656eedfd8d559b4af2efba38c7835031
Michael Uzor <michael.uzor@gmail.com>
2022-09-09 21:25:07 -0700

    numbers

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabet in reverse
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter='z'; letter>='a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
