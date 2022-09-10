047cf1e0aeea1c69f7e92f4b1db5c8a61d225d1d
Michael Uzor <michael.uzor@gmail.com>
2022-09-09 20:58:57 -0700

    all letters excpet e and q"

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints letters except q and e
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter='a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar (letter);
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
