144927de8a3e061b3565432ebc2ca36b37b25983
Michael Uzor <michael.uzor@gmail.com>
2022-09-09 20:30:54 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase
+ *
+ * Return: always 0
+ */
+
+int main (void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar (letter);
+
+	putchar ('\n');
+
+	return (0)
+}
+
