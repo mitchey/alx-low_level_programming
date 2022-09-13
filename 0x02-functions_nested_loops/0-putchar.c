4578f012dbab8a9af01f638f11b7871efe2aa9b0
Michael Uzor <michael.uzor@gmail.com>
2022-09-13 14:41:07 -0700

    print _putchar followed by new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,22 @@
 #include "main.h"
 
-/* main - entry point
+/*
+ * main - entry point
  *
  * Return: always 0
  */
 
 int main(void)
 {
-	_putchar('p');
-	_putchar('\n');
+	_putchar ('_');
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
+	_putchar ('\n');
 
 	return(0);
 }
