337dcda3c064a88116ac092d061ed95894aabdb3
Michael Uzor <michael.uzor@gmail.com>
2022-09-14 14:29:05 -0700

    add integers and return result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - prints and two numbers
+ * @i: print int i
+ * @k: print int k
+ * Return: always 0
+ */
+
+int add(int i, int k)
+{
+	return (i + k);
+}
