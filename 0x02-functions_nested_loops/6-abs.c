c658acf8f8386ff83523a26a5caca4e55bc1eb63
Michael Uzor <michael.uzor@gmail.com>
2022-09-14 13:25:32 -0700

    compute absolute number

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,11 +5,13 @@
  * _abs - computes absolute value
  * abs - absolute value
  * @n: number to print
+ * Return: absolute value
  */
 
 int _abs(int n)
 {
 	int abs_n = abs(n);
+
 	return (abs_n);
 }
 
