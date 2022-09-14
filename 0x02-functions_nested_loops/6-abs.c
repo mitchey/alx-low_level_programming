df4e0d7bae516f3f0e69a0bc3018345ebc42c6bc
Michael Uzor <michael.uzor@gmail.com>
2022-09-14 13:20:05 -0700

    compute absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - computes absolute value
+ * abs - absolute value
+ * @n: number to print
+ */
+
+int _abs(int n)
+{
+	int abs_n = abs(n);
+	return (abs_n);
+}
+
