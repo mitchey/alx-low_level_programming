afb684e7460b50f17fbc9829593caa5cd39e7c7b
Michael Uzor <michael.uzor@gmail.com>
2022-09-16 14:34:29 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 - 9 
+ * Return: always 0
+ */
+
+void print_numbers(void)
+{
+	_putchar i;
+
+	for (i = '1'; i <= '9'; i++)
+	{
+		_putchar (i);
+		_putchar ('\n');
+	}
+	return (0);
+}
