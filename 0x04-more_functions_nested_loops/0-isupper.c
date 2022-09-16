7210af0265ef6543d3ec0635372ed1535a0f2b4f
Michael Uzor <michael.uzor@gmail.com>
2022-09-16 13:54:02 -0700

    check for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase
+ * @c: character to check
+ * Return: always 0
+ */
+
+int _isupper(int c)
+{
+	int i;
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		if (c = i)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef
+#define
+
+int _isupper(int c);
+
+#endif
