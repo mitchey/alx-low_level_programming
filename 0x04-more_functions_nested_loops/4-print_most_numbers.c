9cb3d4bb5571c5ebb386bd2fa88ad90f35ead710
Michael Uzor <michael.uzor@gmail.com>
2022-09-16 14:49:12 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 50 && != 52)
+			_putchar (i);
+	}
+	_putchar('\n');
+}
