58af680c0b80bcf288bc98ab78d3d78e969536c0
Michael Uzor <michael.uzor@gmail.com>
2022-09-20 12:06:32 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * _strlen - length of string
+ * @s: params
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
