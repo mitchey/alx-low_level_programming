91dc97b7747496756ff9188c18f1650eb9af45ff
Michael Uzor <michael.uzor@gmail.com>
2022-09-22 14:40:22 -0700

    encodes a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,13 +11,13 @@ char *leet(char *s)
 	int count = 0, i;
 	int lower_case[] = {97, 101, 111, 116, 108};
 	int upper_case[] = {65, 69,79, 84, 76};
-	int number[] = {52, 51, 48, 55, 49};
+	int numbers[] = {52, 51, 48, 55, 49};
 
 	while (*(s + count) != '\0')
 	{
 		for (i = 0; i < 5; i++)
 		{
-			if (*(s + count) == lower[i] || *(s + count) == upper_case[i])
+			if (*(s + count) == lower_case[i] || *(s + count) == upper_case[i])
 			{
 				*(s + count) = numbers[i];
 				break;
