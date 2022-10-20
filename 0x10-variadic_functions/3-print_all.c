3cb7d8ddfad0940e1a53aef44ad3b4a11de13b63
Michael Uzor <michael.uzor@gmail.com>
2022-10-19 19:52:04 -0700

    function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,9 +13,11 @@ void print_all(const char * const format, ...)
 	const char t_arg[] = "cifs";
 
 	va_start(valist, format);
+	
 	while (format && format[i])
 	{
 		j = 0;
+	
 		while (t_arg[j])
 		{
 			if (format[i] == t_arg[j] && k)
