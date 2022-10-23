6e46281aeff699d38198e070d49803acfed76f4a
Michael Uzor <michael.uzor@gmail.com>
2022-10-23 04:13:03 -0700

    function thay prints all the elements of a ist_t list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,11 +9,11 @@
 size_t print_list(const list_t *h)
 {
 	size_t l_node;
-
 	l_node = 0;
+
 	while (h != NULL)
 	{
-		if(h->str == NULL)
+		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
