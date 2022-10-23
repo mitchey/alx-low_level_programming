5ecb5be78b7a022e95eec2760555c587ff4a5308
Michael Uzor <michael.uzor@gmail.com>
2022-10-23 04:39:09 -0700

    function that returns the number of elements in a list t list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,13 +8,13 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t nelem;
-	nelem = 0;
+	size_t elements = 0;
 
-	while (h != NULL)
+	while (h)
 	{
+		elements++;
 		h = h->next;
-		nelem++;
 	}
-	return (nelem);
+
+	return (elements);
 }
