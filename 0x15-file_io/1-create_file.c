eba7d839bcc8cb3685523ce5f98348ecfba0ec9c
Michael Uzor <michael.uzor@gmail.com>
2022-10-31 21:19:06 -0700

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int open, write, len = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -21,13 +21,13 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	open = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	write = write(open, text_content, len);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
-	if (open == -1 || write == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
-	close(open);
+	close(o);
 
 	return (1);
 }
